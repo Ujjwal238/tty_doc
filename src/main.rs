4078e988f46805809ba44f5e72127edb386f9ad9
Ujjwal238 <ujjwalmishra238@gmail.com>
2025-08-26 12:31:27 +0530

    Opens a Plain Window using egui

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    )
+}
+
+struct MyApp {
+    
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |_ui| {
+          
+        });
+    }
+}
